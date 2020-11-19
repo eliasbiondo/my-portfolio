a0edcee9cd31c639c37a51bb02976c0c005c6564
Elias Biondo <contato@eliasbiondo.com>
2020-11-19 18:51:25 -0300

    Smooth scroll & internal links build

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,3 +1,5 @@
+/* Mobile Menu */
+
 const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
 const modalMenu = document.querySelector('.mobile-menu');
 
@@ -5,10 +7,11 @@ window.onload = function() {
     mobileMenuBtn.addEventListener("click", menuPopup, false);
 }
 
-let isTheMenuOpened = 0;
-function menuPopup(e) {
-    
-    if(isTheMenuOpened == 0) {
+let isTheMenuOpened = false;
+
+function menuPopup() {
+
+    if(isTheMenuOpened == false) {
         mobileMenuBtn.firstElementChild.classList.add('first-line-mobile-menu-rotation');
         mobileMenuBtn.lastElementChild.classList.add('second-line-mobile-menu-rotation');
         mobileMenuBtn.lastElementChild.style.marginTop = '0';
@@ -16,7 +19,7 @@ function menuPopup(e) {
         modalMenu.style.display = 'flex';
         document.body.classList.add('stop-scrolling');
 
-        isTheMenuOpened = 1;
+        isTheMenuOpened = true;
     } else {
         mobileMenuBtn.firstElementChild.classList.remove('first-line-mobile-menu-rotation');
         mobileMenuBtn.lastElementChild.classList.remove('second-line-mobile-menu-rotation');
@@ -25,8 +28,57 @@ function menuPopup(e) {
         modalMenu.style.display = 'none';
         document.body.classList.remove('stop-scrolling');
 
-        isTheMenuOpened = 0;
+        isTheMenuOpened = false;
     }
 
 }
 
+
+/* Desktop Menu */
+
+const menuLinks = document.querySelectorAll('.container .menu-navigation ul a')
+const menuListItems = document.querySelectorAll('.container .menu-navigation ul a li')
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", activeBtn, false)
+})
+
+function activeBtn(event) {
+    const clickedLink = event.target;
+
+    menuListItems.forEach(link => {
+        link.classList.remove('active');
+    })
+
+    clickedLink.classList.add('active');
+}
+
+/* Smooth Scroll */
+
+const internalLinks = document.querySelectorAll('.menu-navigation ul a[href^="#"], .contact-me-btn');
+
+internalLinks.forEach(link => {
+    link.addEventListener("click", softScroll, false);
+} )
+
+function softScroll(event) {
+
+    event.preventDefault();
+
+    let link = event.target.parentNode;
+
+    if (event.target.getAttribute('href') == '#contact-me') {
+        link = event.target;
+    }
+
+    const id = link.getAttribute('href');
+    const section = document.querySelector(id);
+    const distanceFromTop = section.offsetTop;
+
+    window.scroll({
+        top: distanceFromTop - 50,
+        behavior: "smooth"
+    })
+
+
+}
